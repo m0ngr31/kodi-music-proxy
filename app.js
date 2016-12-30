fcf710d69cafe47e30718e3f96a732133a0e27b6
Joe Ipson <joeipson@gmail.com>
2016-12-30 13:33:57 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var url = require('url');
+var app = express();
+
+var forceSsl = function (req, res, next) {
+  if (req.headers['x-forwarded-proto'] !== 'https') {
+    return res.redirect(['https://', req.get('Host'), req.url].join(''));
+  }
+  return next();
+};
+
+app.use(forceSsl);
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+});
+
+app.get('/proxy', function(req, res, next) {
+  var queryData = url.parse(req.url, true).query;
+
+  if(!queryData.file)
+    return next();
+
+  var file = queryData.file;
+  var download = request(file);
+
+  return req.pipe(download).pipe(res);
+});
+
+app.listen(3000, function () {
+  console.log('Kodi Music Proxy listening on port 3000.')
+});
